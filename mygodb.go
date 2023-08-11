7e49e0e2ac7ab823e141a4b84f6f0ca2d8f98c18
AuttakornC <auttakorn.camsoi.2547@gmail.com>
2023-08-11 14:24:08 +0700

    Auto Create

--- mygodb.go
+++ mygodb.go
@@ -23,20 +23,6 @@ func (m *Mygodb) Connect(config Config) error {
 	parsed_config := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", config.Host, config.User, config.Pass, config.DBnm, config.Port)
 	db, err := sql.Open("postgres", parsed_config)
 
-	// data, erro := db.Query("SELECT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND	 tablename  = 'student');")
-	// if erro != nil {
-	// 	fmt.Println(erro)
-	// } else {
-	// 	for data.Next() {
-	// 		var a bool
-	// 		if err := data.Scan(&a); err != nil {
-	// 			fmt.Println(err)
-	// 		} else {
-	// 			fmt.Println(a)
-	// 		}
-
-	// 	}
-	// }
 	if err != nil {
 		fmt.Println("+-----------------------------------------------------+")
 		fmt.Println("| [ MyGoDB ] : Cannot connect to the Database!!       |")
